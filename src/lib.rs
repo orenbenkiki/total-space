a356c33a0a2598529598b9daea6098be50ff63ff
Oren Ben-Kiki <oren@ben-kiki.org>
2021-02-20 14:00:04 +0200

    Add Memoize.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,41 @@
-pub fn value() -> i32 {
-    1
+use num_traits::FromPrimitive;
+use num_traits::ToPrimitive;
+use std::collections::HashMap;
+use std::hash::Hash;
+use std::vec::Vec;
+
+pub struct Memoize<T, I> {
+    by_value: HashMap<T, I>,
+    by_index: Vec<T>,
+}
+
+impl<T: Eq + Hash + Copy + Clone, I: FromPrimitive + ToPrimitive + Copy> Memoize<T, I> {
+    pub fn new() -> Self {
+        Memoize {
+            by_value: HashMap::new(),
+            by_index: Vec::new(),
+        }
+    }
+
+    pub fn store(&mut self, value: T) -> I {
+        match self.by_value.get(&value) {
+            Some(index) => *index,
+            None => {
+                let index = I::from_usize(self.by_value.len()).unwrap();
+                self.by_value.insert(value, index);
+                self.by_index.push(value);
+                index
+            }
+        }
+    }
+
+    pub fn get(&self, index: I) -> T {
+        self.by_index[index.to_usize().unwrap()]
+    }
+}
+
+impl<T: Eq + Hash + Copy + Clone, I: FromPrimitive + ToPrimitive + Copy> Default for Memoize<T, I> {
+    fn default() -> Self {
+        Self::new()
+    }
 }
--- tests/test_model.rs
+++ tests/test_model.rs
@@ -1,8 +1,30 @@
 extern crate total_space;
 
+use std::hash::Hash;
 use total_space::*;
 
+#[derive(PartialEq, Eq, Hash, Copy, Clone, Debug)]
+struct Data {
+    pub i: i32,
+}
+
 #[test]
-fn test_nothing() {
-    assert_eq!(value(), 1);
+fn test_memoize() {
+    let mut memory = Memoize::<Data, u8>::new();
+
+    let first = Data { i: 17 };
+    assert_eq!(first.i, 17);
+    assert_eq!(memory.store(first), 0);
+    assert_eq!(first, memory.get(0));
+
+    let second = Data { i: 11 };
+    assert_eq!(second.i, 11);
+    assert_eq!(memory.store(second), 1);
+    assert_eq!(first, memory.get(0));
+    assert_eq!(second, memory.get(1));
+
+    assert_eq!(memory.store(second), 1);
+    assert_eq!(memory.store(first), 0);
+    assert_eq!(first, memory.get(0));
+    assert_eq!(second, memory.get(1));
 }
